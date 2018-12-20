ba67cfa2ff191773040a17f2b4080b6b0e0c7c9c
Elisa Bourgeois <elisabourgeois.eb@gmail.com>
2018-12-20 18:00:05 +0100

    Forgot the auth.js file...

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -4,15 +4,9 @@ const models = require('../models');
 
 router.post('/signup', (req, res) => {
     const userData = req.body;
-    console.log(Object.values(userData));
     models.user.create(userData).then(user => {
-        console.log(`${user.pseudo} inserted`);
         res.status(200).send(user);
     });
-    // const newUser = new models.user(userData);
-    // newUser.save().then((user)=> {
-    //     res.status(200).json(user);
-    // });
 });
 
 module.exports = router;
