04032b84d5dd135c7d22d5201cb90222676c40d3
Elisa Bourgeois <elisabourgeois.eb@gmail.com>
2018-12-21 03:56:06 +0100

    Creation of the networks component

--- views/src/App.js
+++ views/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
 import "./App.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -9,12 +10,17 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import AppBarHeader from "./components/AppBarHeader";
 import AppBarFooter from "./components/AppBarFooter";
+import Networks from "./components/Networks";
+import "typeface-roboto";
 
 library.add(faHome, faCrown, faEllipsisH);
 
 const primaryBackground = "linear-gradient(45deg, #60c36f, #337dc9)";
 
 const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true
+  },
   overrides: {
     MuiButton: {
       // Name of the component ⚛️ / style sheet
@@ -26,12 +32,23 @@ const theme = createMuiTheme({
   }
 });
 
+const styles = {
+  frame: {
+    marginTop: 150,
+    marginBottom: 70
+  }
+};
+
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <MuiThemeProvider theme={theme}>
         <div className="App">
           <AppBarHeader />
+          {/* <div className={classes.frame}>
+            <Networks />
+          </div> */}
           <AppBarFooter />
         </div>
       </MuiThemeProvider>
@@ -39,4 +56,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ views/src/components/Networks.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { Grid, IconButton } from "@material-ui/core";
+
+const styles = {
+  imgIcon: {
+    height: "3em",
+    width: "auto"
+  }
+};
+
+const apps = [
+  "gmail",
+  "outlook",
+  "facebook",
+  "instagram",
+  "twitter",
+  "snapchat",
+  "youtube",
+  "tiktok",
+  "linkedin",
+  "dailymotion",
+  "netflix",
+  "vimeo"
+];
+
+class Networks extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className="Networks">
+        <Grid container spacing={8}>
+          {apps.map((app, index) => (
+            <Grid key={index} item xs={4}>
+              <IconButton color="inherit">
+                <img
+                  src={require(`../assets/app_icons/black_and_white/${app}.png`)}
+                  alt={app}
+                  className={classes.imgIcon}
+                />
+              </IconButton>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Networks);
