baa4a59b3d02f8cd31eeacfb67a4041e2ca39185
D0C702WH0 <jemyplu@gmail.com>
2018-12-21 10:33:47 +0100

    new panda

--- views/src/components/Reward.js
+++ views/src/components/Reward.js
@@ -4,20 +4,19 @@ import AppBarHeader from "./AppBarHeader";
 import AppBarFooter from "./AppBarFooter";
 import Grid from "@material-ui/core/Grid";
 import { Typography } from "@material-ui/core";
-import baby from "../assets/Panda_1.png";
+import child from "../assets/Panda_2.png";
 
 const styles = theme => ({
   reward: {
     width: 150,
     height: 200,
-    marginTop: 50
+    marginTop: 70
   },
-  text:{
-    fontSize:20,
-    margin:10,
-    marginTop: 30,
-    textAlign:"center"
-
+  text: {
+    fontSize: 20,
+    margin: 10,
+    marginTop: 50,
+    textAlign: "center"
   }
 });
 
@@ -27,14 +26,13 @@ class Reward extends Component {
     return (
       <div className={classes.reward_container}>
         <AppBarHeader />
-        <Grid container  direction="column" justify="center" alignItems="center">
+        <Grid container direction="column" justify="center" alignItems="center">
           <Grid xs={12} item>
-            <img className={classes.reward} alt="reward pic" src={baby} />
+            <img className={classes.reward} alt="reward pic" src={child} />
           </Grid>
           <Grid xs={12} item>
             <Typography className={classes.text} variant="h5" component="h2">
-              Félicitation pour ton inscription ! Ton score est de zéro point.
-              Relève nos défis et fais évoluer Oko
+              Félicitations !<br/> Tu viens de passer niveau 2, Oko est fier de toi
             </Typography>
           </Grid>
         </Grid>
